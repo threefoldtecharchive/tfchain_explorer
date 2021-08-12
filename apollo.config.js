42964216d1ed55c54e28fb8a52f831f7d5710852
MohamedElmdary <engm5081@gmail.com>
2021-08-12 11:10:06 +0200

    apollo.config.js

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: "my-app",
+      url: "https://explorer.devnet.grid.tf/graphql/",
+    },
+    includes: ["src/**/*.vue", "src/**/*.js", "src/**/*.ts"],
+  },
+};

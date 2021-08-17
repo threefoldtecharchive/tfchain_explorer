8d79b807faef45b23a154712a71dfb5ce1de7e5d
MohamedElmdary <engm5081@gmail.com>
2021-08-17 12:50:11 +0200

    Added countryDetails

---
+++ src/components/CountryDetails.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container>
+    <v-list-item>
+      <v-list-item-icon>
+        <v-icon size="40" class="mr-2">mdi-map-outline</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title style="font-size: 30px;">
+          Country Details
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-list width="300">
+      <!-- Flag Item -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            Flag
+          </v-list-item-title>
+        </v-list-item-content>
+        <img :src="src" alt="flag" width="40" />
+      </v-list-item>
+      <v-divider />
+
+      <!-- Name Item -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            Name
+          </v-list-item-title>
+        </v-list-item-content>
+        {{ country.name }}
+      </v-list-item>
+      <v-divider />
+
+      <!-- Region Item -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            Region
+          </v-list-item-title>
+        </v-list-item-content>
+        {{ country.region }}
+      </v-list-item>
+      <v-divider />
+
+      <!-- Subregion Item -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            Subregion
+          </v-list-item-title>
+        </v-list-item-content>
+        {{ country.subregion }}
+      </v-list-item>
+      <v-divider />
+
+      <!-- CODE ISO 3 Item -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            Code ISO 3
+          </v-list-item-title>
+        </v-list-item-content>
+        {{ country.code }}
+      </v-list-item>
+      <v-divider />
+
+      <!-- CODE ISO 3 Item -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            Code ISO 2
+          </v-list-item-title>
+        </v-list-item-content>
+        {{ iso2Code }}
+      </v-list-item>
+      <!-- line -->
+    </v-list>
+  </v-container>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { ICountry } from "@/graphql/api";
+import iso3To2 from "country-iso-3-to-2";
+
+@Component({})
+export default class CountryDetails extends Vue {
+  @Prop({ required: true }) country!: ICountry;
+
+  get iso2Code(): string {
+    return iso3To2(this.country.code);
+  }
+
+  get src(): string {
+    const code: string = this.iso2Code.toLocaleLowerCase();
+    return `https://www.worldatlas.com/r/w425/img/flag/${code}-flag.jpg`;
+  }
+}
+</script>
--- src/components/NodeDetails.vue
+++ src/components/NodeDetails.vue
@@ -8,7 +8,9 @@
   >
     <v-sheet class="text-center" height="90vh" v-if="node">
       <div class="content container">
-        <h2>Node Details</h2>
+        <CountryDetails :country="$store.getters.country(node.countryId)" />
+
+        <!-- <h2>Node Details</h2>
         <p>node - {{ node.nodeId }}</p>
         {{ node }}
         <hr />
@@ -22,11 +24,7 @@
         </p>
         {{ $store.getters.twin(node.twinId) }}
         <hr />
-        <p>
-          node.countryId - {{ node.countryId }} -
-          {{ $store.getters.countries.length }}
-        </p>
-        {{ $store.getters.country(node.countryId) }}
+        <CountryDetails :country="$store.getters.country(node.countryId)" />
         <hr />
         <p>
           node.cityId - {{ node.cityId }} - {{ $store.getters.cities.length }}
@@ -44,7 +42,7 @@
           {{ $store.getters.publicConfigs.length }}
         </p>
         {{ $store.getters.publicConfig(node.publicConfigId) }}
-        <hr />
+        <hr /> -->
       </div>
     </v-sheet>
   </v-bottom-sheet>
@@ -53,8 +51,13 @@
 <script lang="ts">
 import { INode } from "@/graphql/api";
 import { Component, Prop, Vue } from "vue-property-decorator";
+import CountryDetails from "./CountryDetails.vue";
 
-@Component({})
+@Component({
+  components: {
+    CountryDetails,
+  },
+})
 export default class NodeDetails extends Vue {
   @Prop({ required: true }) open!: boolean;
   @Prop({ required: true }) node!: INode;

84325e6da95df7b7bab073b68fb5782ccd592186
Rafy Benjamin <rafy.amgadbenjamin@gmail.com>
2021-11-22 11:35:52 +0200

    validate that the counry name is passed by country code to be displayed on the map #65

--- src/components/LocationDetails.vue
+++ src/components/LocationDetails.vue
@@ -5,7 +5,7 @@
         <v-icon size="40" class="mr-2">mdi-google-maps</v-icon>
       </v-list-item-icon>
       <v-list-item-content>
-        <v-list-item-title style="font-size: 30px;">
+        <v-list-item-title style="font-size: 30px">
           Location Details
         </v-list-item-title>
       </v-list-item-content>
@@ -18,6 +18,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import Map from "./Map.vue";
+import { byCountry } from "country-code-lookup";
 
 @Component({
   components: {
@@ -28,7 +29,11 @@ export default class LocationDetails extends Vue {
   @Prop({ required: false }) country?: string;
 
   onGetMapRef(map: SVGElement) {
-    const country = map.querySelector(`[id=${this.country}]`);
+    const conuntryCode =
+      this.country && this.country?.length > 2
+        ? byCountry(this.country)?.internet
+        : this.country;
+    const country = map.querySelector(`[id=${conuntryCode}]`);
 
     if (country) {
       country.setAttribute("fill", "#ff5252");

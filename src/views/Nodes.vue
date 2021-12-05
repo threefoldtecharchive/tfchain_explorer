da4c0a2e7428a808cd4d6484c25923dade60b21b
MohamedElmdary <engm5081@gmail.com>
2021-12-05 11:14:51 +0200

    Added nodes status filter

--- src/views/Nodes.vue
+++ src/views/Nodes.vue
@@ -45,10 +45,18 @@
     </template>
 
     <template v-slot:table>
-      <v-row align="center" justify="end">
-        Gateways
-        <v-switch v-model="withGateway" class="ml-4"></v-switch>
-      </v-row>
+      <div
+        style="display: flex; flex-direction: column; align-items: flex-end; justify-content: center;"
+      >
+        <div>
+          <v-switch
+            v-model="withGateway"
+            style="margin-bottom: -30px;"
+            label="Gateways"
+          />
+          <v-switch v-model="onlyOnline" label="Online" />
+        </div>
+      </div>
       <v-data-table
         ref="table"
         :loading="$store.getters.loading"
@@ -125,6 +133,7 @@ import ComparisonFilter from "@/components/ComparisonFilter.vue";
 })
 export default class Nodes extends Vue {
   withGateway = false;
+  onlyOnline = false;
 
   headers = [
     { text: "ID", value: "nodeId" },
@@ -230,11 +239,16 @@ export default class Nodes extends Vue {
   ];
 
   getNodes() {
-    const nodes: INode[] = this.$store.getters.filtered_nodes;
-    if (!this.withGateway) {
-      return nodes;
+    let nodes: INode[] = this.$store.getters.filtered_nodes;
+    if (this.withGateway) {
+      nodes = nodes.filter(({ publicConfig }) => publicConfig !== null);
     }
-    return nodes.filter(({ publicConfig }) => publicConfig !== null);
+
+    if (this.onlyOnline) {
+      nodes = nodes.filter(({ status }) => status === true);
+    }
+
+    return nodes;
   }
 
   toggleActive(idx: number) {

b28c4ed35de627f42cf64fb95302273d5a4b7fc2
MohamedElmdary <engm5081@gmail.com>
2022-06-22 10:59:11 +0200

    Fix typo & Add dashboard url in build script

--- scripts/build-env.sh
+++ scripts/build-env.sh
@@ -16,19 +16,23 @@ fi
 
 POLKADOT_URL="wss://tfchain.grid.tf/ws"
 PROXY_URL="https://gridproxy.grid.tf"
+DASHBOARD_URL="https://dashboard.grid.tf/"
 
 case $GQL_URL in
   *"dev"*)
-	PROXY_URL="https://gridproxy.dev.grid.tf"
+	  PROXY_URL="https://gridproxy.dev.grid.tf"
     POLKADOT_URL="wss://tfchain.dev.grid.tf/ws"
+    DASHBOARD_URL="https://dashboard.dev.grid.tf/"
     ;;
   *"test"*)
     PROXY_URL="https://gridproxy.test.grid.tf"
     POLKADOT_URL="wss://tfchain.test.grid.tf/ws"
+    DASHBOARD_URL="https://dashboard.test.grid.tf/"
     ;;
   *"qa"*)
     PROXY_URL="https://gridproxy.qa.grid.tf"
     POLKADOT_URL="wss://tfchain.qa.grid.tf/ws"
+    DASHBOARD_URL="https://dashboard.qa.grid.tf/"
     ;;
 esac
 
@@ -38,6 +42,7 @@ window.configs.gql_url = '$GQL_URL';
 window.configs.proxy_url = '$PROXY_URL';
 window.configs.polkadot_url = '$POLKADOT_URL';
 window.configs.version = '$VERSION';
+window.configs.dashboard_url = '$DASHBOARD_URL';
 "
 
 if [ -e $file ]
